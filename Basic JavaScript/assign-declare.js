7c061ed09295d789f08acfb664b9cad89883c1e6
benup211 <benup211@gmail.com>
2023-09-29 20:44:03 +0545

    add:Basic Javascript assignment and declaration

---
+++ Basic JavaScript/assign-declare.js
@@ -0,0 +1,39 @@
+//This is single line comment
+/*This is multiline 
+    comment*/
+//JavaScript provides eight different data types which are ######undefined, null, boolean, string, symbol, bigint, number, and object#######
+//declare variable by var naming->numbers, letters, and $ or _, but may not contain spaces or start with a number
+var myName;
+//Assignment
+var a;
+a=5;
+//Assigning the Value of One Variable to Another
+var b;
+b=a;
+//Initializing Variables with the Assignment Operator
+var c=9;
+//Declare String Variables
+var myFirstName="Benup"
+var myLastName="Ghimire"
+//JavaScript variables are <--declared-->, they have an initial value of <--undefined-->.
+//eg var a; a=a+1->NaN-not a number
+//eg var b; b=b+"hello"->Undefined i.e. unidentified+unindentified
+
+//CASE SENSATIVE
+// Understanding Case Sensitivity in Variables
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+// Explore Differences Between the var and let Keywords
+//var can redeclare value but let cannot i.e var a=5->var a=55, but let a=5 -> let a=6 //error
+
+// Declare a Read-Only Variable with the const Keyword
+// i.e const val=5; then we cannot reassign value
+// cont variable declaration are generally in uppercase
+
